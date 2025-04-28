de59d62ceb385ad891ace0ffbefc914070920617
KaikSAlves <souzaalveskaik@gmail.com>
2025-04-28 07:46:14 -0300

    ProdutoController - Criado e Testado

--- src/main/java/br/senac/com/ProjetoIntegradorBackend/controller/ProdutoController.java
+++ src/main/java/br/senac/com/ProjetoIntegradorBackend/controller/ProdutoController.java
@@ -3,16 +3,16 @@ package br.senac.com.ProjetoIntegradorBackend.controller;
 import java.net.URI;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.senac.com.ProjetoIntegradorBackend.model.Usuario;
-import br.senac.com.ProjetoIntegradorBackend.service.UsuarioService;
+import br.senac.com.ProjetoIntegradorBackend.model.Produto;
+import br.senac.com.ProjetoIntegradorBackend.service.ProdutoService;
+import jakarta.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,56 +24,59 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 
-@RequestMapping("/usuario")
+
+@RequestMapping("/produto")
 @RestController
-public class UsuarioController {
+public class ProdutoController {
     
     @Autowired
-    private UsuarioService s;
+    private ProdutoService s;
+
 
     //post
 
     @PostMapping
-    public ResponseEntity<Usuario> salvar (@RequestBody Usuario u) {
-        u = s.save(u);
+    public ResponseEntity<Produto> salvar(@RequestBody Produto p) {
+        p = s.save(p);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-            .buildAndExpand(u.getId()).toUri();
-        return ResponseEntity.created(uri).body(u);
+          URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(p.getId()).toUri();
+        return ResponseEntity.created(uri).body(p);
     }
 
     //gets
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> pegarPorId(@PathVariable Integer id) {
-        Usuario u = s.findById(id);
+    public ResponseEntity<Produto> pegarPorId(@PathVariable Integer id) {
+        Produto p  = s.findById(id);
 
-        return ResponseEntity.ok().body(u);
+        return ResponseEntity.ok().body(p);
     }
+
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> pegarTodos() {
-        List<Usuario> usuarios = s.findAll();
+    public ResponseEntity<List<Produto>> pegarTodos() {
+        List<Produto> produtos = s.findAll();
 
-        return ResponseEntity.ok().body(usuarios);
+        return ResponseEntity.ok().body(produtos);
     }
-
+    
     //put
 
     @PutMapping("/{id}")
-    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Integer id, @RequestBody Usuario u) {
-        u = s.update(id, u);
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable Integer id, @RequestBody Produto p) {
+        p = s.update(id, p);
+
+        return ResponseEntity.ok().body(p) ;
     }
 
     //delete
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarUsuario(@PathVariable Integer id){
+    public ResponseEntity<Void> deletarProduto(@PathVariable Integer id){
         s.deleteById(id);
+
         return ResponseEntity.noContent().build();
     }
-    
-    
     
 }
